8e6734472122b7981be09b2fac14ceccec8b1e53
Kacper Smaga <kacper.smaga@onet.pl>
2025-03-25 18:05:47 +0100

    feat(api): add admin quiz controller with create, get and patch endpoints

---
+++ WebApi/Controllers/ApiQuizAdminController.cs
@@ -0,0 +1,69 @@
+﻿using ApplicationCore.Interfaces.AdminService;
+using ApplicationCore.Models.QuizAggregate;
+using Microsoft.AspNetCore.JsonPatch;
+using Microsoft.AspNetCore.Mvc;
+using WebApi.Dto;
+
+namespace WebApi.Controllers;
+
+[ApiController]
+[Route("api/admin/quizzes")]
+public class ApiQuizAdminController : ControllerBase
+{
+    private readonly IQuizAdminService _service;
+
+    public ApiQuizAdminController(IQuizAdminService service)
+    {
+        _service = service;
+    }
+
+    [HttpPost]
+    public ActionResult<object> AddQuiz([FromServices] LinkGenerator link, [FromBody] NewQuizDto dto)
+    {
+        var quiz = _service.AddQuiz(new Quiz { Title = dto.Title });
+
+        var url = link.GetPathByAction(
+            HttpContext,
+            nameof(GetQuiz),
+            null,
+            new { quizId = quiz.Id }
+        );
+
+        return Created(url, quiz);
+    }
+
+    [HttpGet("{quizId}")]
+    public ActionResult<Quiz> GetQuiz(int quizId)
+    {
+        var quiz = _service.FindAllQuizzes().FirstOrDefault(q => q.Id == quizId);
+        return quiz is null ? NotFound() : Ok(quiz);
+    }
+    
+    [HttpPatch]
+    [Route("{quizId}")]
+    [Consumes("application/json-patch+json")]
+    public ActionResult<Quiz> AddQuizItem(int quizId, JsonPatchDocument<Quiz>? patchDoc)
+    {
+        var quiz = _service.FindAllQuizzes().FirstOrDefault(q => q.Id == quizId);
+        if (quiz is null || patchDoc is null)
+        {
+            return NotFound(new
+            {
+                error = $"Quiz width id {quizId} not found"
+            });
+        }
+        int previousCount = quiz.Items.Count;
+        patchDoc.ApplyTo(quiz, ModelState);
+        if (!ModelState.IsValid)
+        {
+            return BadRequest(ModelState);
+        }
+        if (previousCount < quiz.Items.Count)
+        {
+            QuizItem item = quiz.Items[^1];
+            quiz.Items.RemoveAt(quiz.Items.Count - 1);
+            _service.AddQuizItemToQuiz(quizId, item);
+        }
+        return Ok(_service.FindAllQuizzes().FirstOrDefault(q => q.Id == quizId));
+    }
+}
---
+++ WebApi/Dto/NewQuizDto.cs
@@ -0,0 +1,10 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebApi.Dto;
+
+public class NewQuizDto
+{
+    [Required]
+    [StringLength(200, MinimumLength = 3)]
+    public string Title { get; set; }
+}
